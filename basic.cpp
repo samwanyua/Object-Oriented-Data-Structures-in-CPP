fa6973f9b9ed754c70bc70b3b8027df195932d66
samwanyua <samexample8@gmail.com>
2024-03-01 19:04:46 +0300

    Basic content

---
+++ basic.cpp
@@ -0,0 +1,33 @@
+/*
+Primitive data types
+    int
+    char
+    bool
+    float
+    double
+    void - denotes absence of a value
+
+User-defined data types
+    std::string - string (seuence of characters)
+    std::vector - a dynamically growing array
+
+    int main(){retun 0;} // 0 - successful program, non-zero - errors
+
+    cout - console out
+    endl - end line
+
+    header file(.h) -defines the interface to a class, includes
+        - declaration of all member variables
+        - declaration of all member functions
+
+    standard library
+        iostream - includes operations for reading/writing files and console
+        namespace - allows us to avoid name conflicts for commonly used names
+
+        uiuc namespace - user-defined namespace
+        
+
+
+
+*/
+
